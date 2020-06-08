b4ae7497e37b7b64373f89bf657b1e02dd5f6cb6
Judy <kimjudyh@gmail.com>
2020-06-08 10:54:40 -0700

    started BirdModel and api request methods

---
+++ src/models/BirdModel.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const birdEndpoint = 'http://localhost:4000/api/v1/bird/';
+
+// make an instance of axios that enables passing of cookies
+const cookieAxios = axios.create({
+  withCredentials: true
+})
+
+export default class BirdModel {
+  // index route
+  // GET '/birdingSession/:birdingSessionId'
+  static all = (birdingSessionId) => {
+    const request = cookieAxios.get(`${birdEndpoint}/birdingSession/${birdingSessionId}`)
+    return request;
+  }
+  
+  // create new bird
+  // POST '/birdingSession/:birdingSessionId
+
+  // get one bird
+  // GET '/:birdingSessionId/bird/:id'
+
+  // update a bird
+  // PUT '/:birdingSessionId/bird/:id'
+
+  // delete a bird
+  // DELETE '/:birdingSessionId/bird/:id'
+
+}
