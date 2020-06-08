a1c9ea71c75db380c348f4fe4355fa0911b44c73
Judy <kimjudyh@gmail.com>
2020-06-07 23:17:52 -0700

    added logout functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import React, { useState } from 'react';
 import { withRouter } from 'react-router-dom';
 import Routes from './config/routes';
+import Navbar from './components/Navbar';
+import UserModel from './models/UserModel';
 import './App.css';
 
 function App(props) {
+  const [currentUser, setCurrentUser] = useState(localStorage.getItem('uid'));
+
+  // store the currently logged in user in local storage
+  const storeUser = (userId) => {
+    setCurrentUser(userId);
+    localStorage.setItem('uid', userId);
+  }
+
+  // logout
+  const logout = (event) => {
+    event.preventDefault();
+    localStorage.removeItem('uid');
+    UserModel.logout()
+      .then(res => {
+        console.log(res);
+        setCurrentUser(null);
+      })
+      .catch(err => console.log(err))
+  }
+
   return (
     <div className="App">
+      <Navbar 
+        currentUser={currentUser}
+        logout={logout}
+      />
       Hello
-      <Routes />
+      <Routes 
+        currentUser={currentUser}
+        storeUser={storeUser}
+      />
     </div>
   );
 }
