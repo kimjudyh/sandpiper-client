9a10c47250dadfc8ec3a100154ab9a946caad7e6
Judy <kimjudyh@gmail.com>
2020-06-08 10:52:43 -0700

    moved fetch data into function

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,30 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import BirdingSessionModel from '../models/BirdingSessionModel';
+import BirdingSessionContainer from '../containers/BirdingSessionContainer';
 
 const Profile = (props) => {
-  const [birdingSessions, setBirdingSessions] = useState([]);
-
-  useEffect(() => {
-    BirdingSessionModel.all()
-      .then(res => {
-        console.log(res.data.allBirdingSessions);
-        if (birdingSessions !== res.data.allBirdingSessions) {
-          setBirdingSessions(res.data.allBirdingSessions);
-        }
-      })
-      .catch(err => console.log(err))
-      // TODO: fix infinite update loop when running effect on birdingSessions change
-      // reload when user logs out
-  }, []);
-
-  const birdingSessionLocations = birdingSessions.map((element, index) => (
-    <h2 key={index}>{element.location}</h2>
-  ))
 
   return (
     <div>
       Profile
-      {birdingSessionLocations}
+      <BirdingSessionContainer />
     </div>
   )
 }
