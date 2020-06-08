6cda2d44c1ea13c5caa04997e054ec84df6e4bb0
Judy <kimjudyh@gmail.com>
2020-06-07 23:17:36 -0700

    added working login page

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-
+// Import Components
 import Register from '../pages/Register';
+import Login from '../pages/Login';
 
 const Routes = (props) => (
   <Switch>
     <Route exact path='/register' component={ Register } />
+    <Route exact path='/login' render={(routeComponentProps) => {
+      // pass props to Login component using render, callback fcn
+      return <Login 
+        // pass it router props such as history, match, etc.
+        {...routeComponentProps}
+        currentUser={props.currentUser}
+        storeUser={props.storeUser}
+      />
+    }}/>
   </Switch>
 )
 
---
+++ src/pages/Login.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import UserModel from '../models/UserModel';
+
+const Login = (props) => {
+  const [userData, setUserData] = useState({
+    email: '',
+    password: ''
+  });
+  // render a form 
+  // want to update the form inputs when changes are made to it
+  const handleChange = (event) => {
+    // setUserData overwrites the state, instead of merging
+    // so save the current state to an object and change one field
+    let newState = Object.assign({}, userData);
+    newState[event.target.name] = event.target.value;
+    setUserData(newState);
+    // update the userData state
+  }
+
+  // send a login request to API when submit form
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // make API request
+    UserModel.login(userData)
+      .then(data => {
+        console.log(data);
+        if (data.status === 200) {
+          // send to profile page
+        } else {
+          // say invalid credentials
+        }
+        // reset form inputs
+        setUserData({
+          email: '',
+          password: '',
+        })
+      })
+  }
+
+  return (
+    <div>
+      Login Page
+      <form onSubmit={ handleSubmit }>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input 
+            type="text"
+            id="email"
+            name="email"
+            value={ userData.email }
+            onChange={ handleChange }
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input 
+            type="password"
+            id="password"
+            name="password"
+            value={ userData.password } 
+            onChange={ handleChange }
+          />
+        </div>
+        <button type="submit">Log In</button>
+      </form>
+    </div>
+  )
+}
+
+export default Login;
