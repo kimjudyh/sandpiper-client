e73fec47d0d7565a2fab7a2d6df27fd759c8c1ac
Judy <kimjudyh@gmail.com>
2020-06-08 17:16:49 -0700

    added a nicer bootstrap navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,25 +3,36 @@ import { Link } from 'react-router-dom';
 
 const Navbar = (props) => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to={'/login'}>Login</Link>
+<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div className="container">
+    <Link className="navbar-brand" to={'/register'}>Register</Link>
+    <Link className="navbar-brand" to={'/login'}>Login</Link>
+    <Link className="navbar-brand" to={'/profile'}>Profile</Link>
+    <Link className="navbar-brand" to={'/logout'} onClick={props.logout}>Logout</Link>
+    {/* Hamburger Menu Button */}
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    {/* Links in dropdown */}
+    <div className="collapse navbar-collapse" id="navbarsExample07">
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-item active">
+          <Link className="nav-link" to={'/birdingSession/new'}>New Birding Session</Link>
         </li>
-        <li>
-          <Link to={'/register'}>Register</Link>
+        <li className="nav-item">
+          <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
         </li>
-        <li>
-          <Link to={'/logout'} onClick={props.logout}>Logout</Link>
-        </li>
-        <li>
-          <Link to={'/profile'}>Profile</Link>
-        </li>
-        <li>
-          <Link to={'/birdingSession/new'}>New Birding Session</Link>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" id="dropdown07" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+          <div className="dropdown-menu" aria-labelledby="dropdown07">
+            <a className="dropdown-item" href="#">Action</a>
+          </div>
         </li>
       </ul>
-    </nav>
+    </div>
+  </div>
+</nav>
+
   )
 }
 
