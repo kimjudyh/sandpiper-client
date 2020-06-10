9d7e9b3e9e104e3b7832eefb4e2d1a0429a68110
Judy <kimjudyh@gmail.com>
2020-06-10 12:35:55 -0700

    fixed Profile page to when logged out

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,17 +3,27 @@ import BirdingSessionContainer from '../containers/BirdingSessionContainer';
 
 const Profile = (props) => {
 
-  return (
-    <div className="container">
-      <h3>{props.currentUser.name}'s Profile</h3>
-      <BirdingSessionContainer 
-        {...props.routeComponentProps}
-        currentUser={props.currentUser}
-        storeUser={props.storeUser}
-      />
+  // if (!props.currentUser.uid) {
+  //   props.routeComponentProps.history.push('/');
+  // }
 
-    </div>
-  )
+  if (props.currentUser.uid) {
+    return (
+      <div className="container">
+        <h3>{props.currentUser.name}'s Profile</h3>
+        <BirdingSessionContainer 
+          {...props.routeComponentProps}
+          currentUser={props.currentUser}
+          storeUser={props.storeUser}
+        />
+
+      </div>
+    )
+  } else {
+    return (
+      <h1>Not Logged In</h1>
+    )
+  }
 }
 
 export default Profile;
