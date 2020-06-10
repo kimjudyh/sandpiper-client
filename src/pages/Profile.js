166eb1b3806a5fbd461266b296373cf7f72736fe
Judy <kimjudyh@gmail.com>
2020-06-09 23:14:39 -0700

    removed unused code, imports from Profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import BirdingSessionModel from '../models/BirdingSessionModel';
+import React from 'react';
 import BirdingSessionContainer from '../containers/BirdingSessionContainer';
-import NewBirdingSessionForm from '../forms/NewBirdingSessionForm';
 
 const Profile = (props) => {
 
   return (
-    <div>
-      <h3>Profile</h3>
+    <div className="container">
+      <h3>{props.currentUser.name}'s Profile</h3>
       <BirdingSessionContainer 
         {...props.routeComponentProps}
         currentUser={props.currentUser}
@@ -19,19 +17,3 @@ const Profile = (props) => {
 }
 
 export default Profile;
-
-// export const useFormDisplay = () => {
-//   const [formDisplay, setFormDisplay] = useState({display: 'none'})
-//   const toggleFormDisplay = () => {
-//     // toggle show form state
-//     if (formDisplay.display === 'none') {
-//       setFormDisplay({display: 'block'})
-//     } else {
-//       setFormDisplay({display: 'none'})
-//     }
-//   }
-//   return ({
-//     formDisplay,
-//     toggleFormDisplay
-//   })
-// }
