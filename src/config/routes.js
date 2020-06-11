228ce24fc233b15680aee8527cd8bc29f81261aa
Judy <kimjudyh@gmail.com>
2020-06-11 13:11:42 -0700

    added about, home page

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 // Import Pages
+import Home from '../pages/Home';
+import About from '../pages/About';
 import Register from '../pages/Register';
 import Login from '../pages/Login';
 import Profile from '../pages/Profile';
@@ -10,6 +12,8 @@ import PhotoList from '../components/PhotoList';
 
 const Routes = (props) => (
   <Switch>
+    <Route exact path='/' component={ Home }/>
+    <Route exact path='/about' component={ About }/>
     <Route exact path='/register' component={ Register } />
     <Route exact path='/login' render={(routeComponentProps) => {
       // pass props to Login component using render, callback fcn
