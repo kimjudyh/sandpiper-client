4a0d12ba9859ca80ab20c0e9df9de96f5bccfe83
Judy <kimjudyh@gmail.com>
2020-06-15 00:34:35 -0700

    added content to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,8 +9,54 @@ const Home = () => {
       <div className="home-image-frame">
         <img className="home-image" src={sandpiperImage} alt="sandpiper walking on beach"/>
       </div>
-      <Link to={'/register'}><h3>Sign Up</h3></Link>
-      <Link to={'/about'}><h4>About</h4></Link>
+      <div className="home-links">
+        <Link to={'/register'}><h3>Sign Up</h3></Link>
+        <Link to={'/login'}><h3>Login</h3></Link>
+      </div>
+      <div className="home-info">
+        <p>
+          Sandpiper is an app where you can keep track of the birds you saw and the photos you took of them. You can share entries, or birding sessions, with other users that went birding with you. Compile the photos that might have otherwise lived on separate devices in the same spot. 
+        </p>
+        <Link to={'/about'}><h4>About</h4></Link>
+      </div>
+      <div className="home-info">
+        <p>Share a birding session with registered users</p>
+        <p>Add photos to a specific bird in a birding session</p>
+        <p>View all photos you've added, and that have been shared with you, and sort them by date uploaded, birding session, bird, or behavior</p>
+        <Link to={'/register'}><h3>Sign Up</h3></Link>
+      </div>
+      <div className="home-info">
+        <p>
+        Sandpiper is responsive to mobile device screens. Sign up to add birding sessions that contain birds you've seen and the photos you've taken of them.
+        </p>
+        <span>
+          <img src={process.env.PUBLIC_URL + '/readme_images/mobile-profile.png'} height='500' />   <img src={process.env.PUBLIC_URL + '/readme_images/mobile-birding-session.png'} height='500' />
+        </span>
+        <Link to={'/register'}><h3>Sign Up</h3></Link>
+      </div>
+      <div className="home-info">
+        <p>
+          Any session can be shared with a user that has signed up for Sandpiper. The birding session contains a map. In future work, the user will be able to drop pins and utilize photo metadata.
+        </p>
+        <span>
+          <img src={process.env.PUBLIC_URL + '/readme_images/mobile-birding-session-share.png'} height='500' />   <img src={process.env.PUBLIC_URL + '/readme_images/mobile-birding-session-map.png'} height='500' />
+        </span>
+        <Link to={'/register'}><h3>Sign Up</h3></Link>
+      </div>
+      <div className="home-info">
+        <p>
+          Photos and field notes can be added to each bird in a birding session. All photos can be viewed and sorted on a separate page.
+        </p>
+        <span>
+          <img src={process.env.PUBLIC_URL + '/readme_images/mobile-bird.png'} height='500' />   <img src={process.env.PUBLIC_URL + '/readme_images/mobile-photos-sorted-bird.png'} height='500'></img>
+        </span>
+        <Link to={'/register'}><h3>Sign Up</h3></Link>
+      </div>
+      <div className="home-info">
+        <p>
+          Sandpiper works best in Chrome, Firefox, Edge, and currently doesn't work in Safari for some reason.
+        </p>
+      </div>
     </div>
   )
 }
